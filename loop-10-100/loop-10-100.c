bd32c1b92f303973e4fb557d43a085eb9a835337
Martin Hlavacek <martin@martinhlavacek.cz>
2025-11-27 17:27:47 +0100

    update

--- loop-10-100/loop-10-100.c
+++ loop-10-100/loop-10-100.c
@@ -5,7 +5,7 @@ int main() {
     // Cyklus for, který začíná od 10 a pokračuje dokud je 'i' menší nebo rovno 100
     // i += 10 znamená, že se 'i' zvyšuje o 10 v každé iteraci
     // a jedna se o zkraceny zapis pro i = i + 10
-    for (i = 10; i <= 100; i += 10) {
+    for (i = 10; i <= 100; i = i + 10) {
         printf("%d\n", i); // Vypíše aktuální hodnotu 'i'
     }
 
